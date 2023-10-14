96d1575f536e0d05e212e3f22fcaba5edde3cf5f
mensenvau <otkir8311@gmail.com>
2023-10-14 17:25:11 +0500

    add

--- front-end/config/site.js
+++ front-end/config/site.js
@@ -2,8 +2,8 @@
 export const siteConfig = {
     name: "SMARTJOB",
     description: "SMARTJOB",
-    // apiURL: "https://sm-api.datatech.uz/",
-    apiURL: "http://localhost:6002/",
+    apiURL: "https://sm-api.datatech.uz/",
+    // apiURL: "http://localhost:6002/",
     email: {
         support: 'support@mycontest.dev',
     },
