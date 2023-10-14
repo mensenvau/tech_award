c0136184f58161f94ebb21059d3c669419d98970
mensenvau <otkir8311@gmail.com>
2023-10-14 13:00:44 +0500

    add

--- front-end/app/(marketing)/page.js
+++ front-end/app/(marketing)/page.js
@@ -1,8 +1,9 @@
 "use client"
 
 import Link from "next/link";
-import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import { siteConfig } from "@/config/site";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { useRouter } from 'next/navigation'
 import { useEffect } from "react";
 
@@ -21,18 +22,16 @@ export default function Home() {
           <div className="mx-auto max-w-2xl py-20 sm:py-35 lg:py-40">
             <div className="text-center">
               <h1 className="text-4xl font-bold tracking-tight dark:text-white-600 sm:text-5xl">
-                Code, Interview, and Learn together with SMARTJOB
+                Orzudagi ishni birga topamiz!
               </h1>
               <p className="mt-6 text-lg leading-8 dark:text-slate-300 text-gray-600">
-                We help companies develop the strongest tech teams around. We help candidates sharpen their tech skills and pursue job opportunities.
+                Smart Jobs sizga ish joyingizni topishda ko’maklashadi.
               </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6">
-                <Link href={siteConfig.signUp} className="rounded-md px-3.5 py-2.5 text-sm font-medium text-white shadow-sm bg-gray-950	dark:bg-sky-700">
-                  Get started
-                </Link>
-                <Link href="/c/about" className="flex rounded-md px-3 py-2 text-sm font-semibold dark:text-white outline dark:outline-sky-700">
-                  About Us <ChevronRightIcon className="w-5 h-5" />
-                </Link>
+              <div className="mt-10 w-11/12 flex items-center justify-center gap-x-6">
+                <div className="flex w-10/12 max-w-sm items-center space-x-2">
+                  <Input type="email" placeholder="Email" />
+                  <Button type="submit">Subscribe</Button>
+                </div>
               </div>
             </div>
           </div>
